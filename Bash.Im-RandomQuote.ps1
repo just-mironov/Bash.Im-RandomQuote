91fb76bc86de75d4c3e452cb1372bb46724b9b5b
slev1n <am@vidnoe.net>
2020-02-18 14:54:10 +0300

    Update Bash.Im-RandomQuote.ps1

--- Bash.Im-RandomQuote.ps1
+++ Bash.Im-RandomQuote.ps1
@@ -1,42 +1,33 @@
 <#
     Выводим случайную цитату с сайта bash.im из "Лучшего" за последние 5 лет.
-
     Чтобы цитата выводилась при запуске PowerShell - нужно добавить весь код из файла в файл профиля.
     Открыть файл профиля (powershell команда):
     notepad $profile
-
     Если выходит ошибка, значит файла профиля еще нет - нужно его создать:
     New-Item -path $profile -type file -force
-
     После чего открыть, добавить код и сохранить
 #>
 
-Add-Type -AssemblyName System.Web
-$utils = [Web.HttpUtility]
-$wc = New-Object Net.WebClient
 $quoteslist = New-Object Collections.ArrayList
-
 $cur_year = Get-Date -uformat %Y
 $old_year = $cur_year - 5
 
-$url = "http://bash.im/bestyear/"
+$url = "http://bash.im/best/"
 
 for ([int] $i = $cur_year; $i -gt $old_year; $i--)
 {
-    $page = $wc.DownloadString($url + $i)
-    $matches = [regex]::Matches($page, "<div class=""text"">(.*?)</div>")
+    $page = $url + $i
+    $WebResponse = (Invoke-WebRequest -Uri $page -UserAgent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36")
 
-    #$matches | ForEach-Object { $count = $quoteslist.Add($utils::HtmlDecode($_.Groups[1].Value.Replace("<br>", "`r`n"))) }
-
-    ForEach ($match in $matches)
-    {
-        $count = $quoteslist.Add($utils::HtmlDecode($match.Groups[1].Value.Replace("<br>", "`r`n")))
-    }
+    $quotes = ($WebResponse.ParsedHtml.DocumentElement.GetElementsByTagName('div') | Where { $_.ClassName -match '\bquote__body\b'}).InnerText
+    $add = $quoteslist.Add($quotes)
 }
 
 Function Bash # Функция, чтобы можно было вручную выводить случайную цитату
 {
-  Write-Host $quoteslist[(Get-Random $quoteslist.Count)] -foreground Yellow
+  $randomYear = get-random $quoteslist.Count
+  $randomQuote = get-random $quoteslist[$randomYear].Count
+  Write-Host $quoteslist[$randomYear].Get($randomQuote) -foreground Yellow
   echo ""
 }
 
